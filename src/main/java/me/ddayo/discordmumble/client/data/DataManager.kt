f58796e7fcab5424eb5218a30c1ace31530405b2
dayo <dayo05@daum.net>
2022-02-14 21:28:09 +0900

    Remove network class

--- src/main/java/me/ddayo/discordmumble/client/data/DataManager.kt
+++
@@ -1,9 +0,0 @@
-package me.ddayo.discordmumble.client.data
-
-import me.ddayo.discordmumble.network.NetworkData
-
-class DataManager {
-    companion object {
-        var currentNetworkData = NetworkData()
-    }
-}
--- src/main/java/me/ddayo/discordmumble/network/NetworkData.kt
+++
@@ -1,8 +0,0 @@
-package me.ddayo.discordmumble.network
-
-import me.ddayo.discordmumble.client.discord.DiscordUserData
-
-class NetworkData {
-    val participantList = emptyMap<Long, DiscordUserData>()
-    val channelList = emptyMap<String, List<Long>>()
-}
