c173be3c8f3c3dfd7b4339ea7d81f50d66732ada
dayo <dayo05@daum.net>
2022-02-16 21:38:11 +0900

    Add selection of native shared libraries

--- src/main/java/me/ddayo/tritone/Tritone.kt
+++ src/main/java/me/ddayo/tritone/Tritone.kt
@@ -10,6 +10,7 @@ import net.minecraft.client.Minecraft
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent
 import net.minecraftforge.fml.common.Mod
+import org.apache.commons.lang3.SystemUtils
 import org.apache.logging.log4j.LogManager
 import java.io.File
 
@@ -25,8 +26,17 @@ class Tritone {
     init {
         //System.loadLibrary("discord_game_sdk")
         //System.loadLibrary("native")
-        System.load(File(Minecraft.getInstance().gameDir, "mods/discord_game_sdk.dll").canonicalPath)
-        System.load(File(Minecraft.getInstance().gameDir, "mods/native.dll").canonicalPath)
+        if(SystemUtils.IS_OS_WINDOWS) {
+            System.load(File(Minecraft.getInstance().gameDir, "mods/discord_game_sdk.dll").canonicalPath)
+            System.load(File(Minecraft.getInstance().gameDir, "mods/native.dll").canonicalPath)
+        }
+        if(SystemUtils.IS_OS_LINUX) {
+            System.load(File(Minecraft.getInstance().gameDir, "mods/discord_game_sdk.so").canonicalPath)
+            System.load(File(Minecraft.getInstance().gameDir, "mods/native.so").canonicalPath)
+        }
+        else {
+            throw IllegalStateException("Not supported OS")
+        }
         DiscordAPI.initialize()
         FMLJavaModLoadingContext.get().modEventBus.addListener { event: FMLCommonSetupEvent -> setup(event) }
         MinecraftForge.EVENT_BUS.register(this)
