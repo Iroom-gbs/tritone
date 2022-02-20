7fdcff8f755ec9e3dc664a0330293514b16b82bb
dayo <dayo05@daum.net>
2022-02-20 20:25:08 +0900

    Add connecting overlay

--- src/main/java/me/ddayo/tritone/client/ClientEvent.kt
+++ src/main/java/me/ddayo/tritone/client/ClientEvent.kt
@@ -1,18 +1,17 @@
 package me.ddayo.tritone.client
 
+import com.mojang.blaze3d.matrix.MatrixStack
 import me.ddayo.tritone.client.util.MinecraftStringUtil
+import me.ddayo.tritone.client.util.RenderUtil
 import me.iroom.tritone.DiscordAPI
-import me.iroom.tritone.util.MathUtil
 import me.iroom.tritone.util.Vector3D
 import net.minecraft.client.Minecraft
+import net.minecraftforge.client.event.GuiScreenEvent
 import net.minecraftforge.client.event.RenderGameOverlayEvent
-import net.minecraftforge.client.event.RenderPlayerEvent
-import net.minecraftforge.client.event.RenderWorldLastEvent
-import net.minecraftforge.event.entity.EntityJoinWorldEvent
-import net.minecraftforge.event.world.WorldEvent
 import net.minecraftforge.eventbus.api.EventPriority
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import org.apache.logging.log4j.LogManager
+import org.lwjgl.opengl.GL21.*
 
 class ClientEvent {
 
@@ -24,17 +23,14 @@ class ClientEvent {
         if(Minecraft.getInstance().player != null) {
             val cloned = HashSet<String>(DiscordAPI.managedPlayer).toMutableSet()
             for (x in Minecraft.getInstance().world?.players!!) {
-                logger.info(x.uniqueID)
                 if(x.uniqueID == Minecraft.getInstance().player!!.uniqueID) continue
                 val p = Minecraft.getInstance().player!!.positionVec
                 val o = x.positionVec
 
                 if (cloned.contains(x.uniqueID.toString())) {
-                    logger.info("${x.uniqueID} ${x.name.string}")
                     cloned.remove(x.uniqueID.toString())
                     DiscordAPI.setVolume(DiscordAPI.voicePlayerList[x.uniqueID.toString()]!!, Vector3D(p.x, p.y, p.z), Vector3D(o.x, o.y, o.z))
                 } else if (DiscordAPI.voicePlayerList.containsKey(x.uniqueID.toString())) {
-                    logger.info("${x.uniqueID} ${x.name.string}")
                     MinecraftStringUtil.nameCache[x.uniqueID] = x.name.string
                     DiscordAPI.managedPlayer.add(x.uniqueID.toString())
                     DiscordAPI.setVolume(DiscordAPI.voicePlayerList[x.uniqueID.toString()]!!, Vector3D(p.x, p.y, p.z), Vector3D(o.x, o.y, o.z))
@@ -53,9 +49,26 @@ class ClientEvent {
         DiscordAPI.tick()
     }
 
+    private fun renderOverlay() {
+        if(DiscordAPI.currentLobby == 0L) {
+            RenderUtil.bindTexture("testwhite.png")
+            RenderUtil.renderRect(0, 0, 60, 25)
+            Minecraft.getInstance().fontRenderer.drawString(MatrixStack(), "연결중입니다...", 0.0f, 0.0f, 0x000000)
+        }
+    }
+
     @SubscribeEvent(priority = EventPriority.HIGH)
-    fun onRd(event: net.minecraftforge.client.event.GuiScreenEvent.DrawScreenEvent.Pre) {
+    fun onRd(event: GuiScreenEvent.DrawScreenEvent.Pre) {
         recalculateDist()
         DiscordAPI.tick()
     }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    fun renderOverlay(event: RenderGameOverlayEvent.Post) {
+        if(event.type == RenderGameOverlayEvent.ElementType.AIR)
+            renderOverlay()
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    fun renderOverlay(event: GuiScreenEvent.DrawScreenEvent.Post) = renderOverlay()
 }
