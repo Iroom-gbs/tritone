878559059f023223f6593171c8baa3488bff734c
dayo <dayo05@daum.net>
2022-02-20 11:04:51 +0900

    Make to uuid format via regex

--- src/main/java/me/ddayo/tritone/client/discord/ForgeDiscordEvent.kt
+++ src/main/java/me/ddayo/tritone/client/discord/ForgeDiscordEvent.kt
@@ -4,5 +4,7 @@ import me.iroom.tritone.DiscordEvent
 import net.minecraft.client.Minecraft
 
 class ForgeDiscordEvent: DiscordEvent() {
-    override fun getMCName() = Minecraft.getInstance().session.playerID
+    override fun getMCName() = Minecraft.getInstance().session.playerID.replaceFirst(
+            "(\\p{XDigit}{8})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}+)".toRegex(), "$1-$2-$3-$4-$5"
+    )
 }
